085df19a1f294fee8a58f13a32f4fb626306d8f0
민경호 <1736s@naver.com>
2024-08-01 11:26:53 +0900

    chore: 배민에서 사용할 assets 추가

---
+++ src/shared/assets/icons.ts
@@ -0,0 +1,26 @@
+const ICONS = {
+  배민: {
+    APP_STORE: "/appstore.png",
+    GOOGLE_PLAY: "/googleplay.png",
+    ICON_BLOG: "/iconBlog.png",
+    ICON_FACEBOOK: "/iconFacebook.png",
+    ICON_INSTAGRAM: "/iconInstagram.png",
+    ICON_YOUTUBE: "/iconYoutube.png",
+    LOGO: "/logo.png",
+    MAIN_TEXT_01: "/main_text_01.png",
+    MAIN_TEXT_02: "/main_text_02.png",
+    MAIN_TEXT_03: "/main_text_03.png",
+    MAIN_TEXT_04: "/main_text_04.png",
+    MAIN_TEXT_05: "/main_text_05.png",
+    MAIN_01: "/main01.jpg",
+    MAIN_02: "/main02.jpg",
+    MAIN_03: "/main03.jpg",
+    MAIN_04: "/main04.jpg",
+    MAIN_05: "/main05.jpg",
+    QR_CODE: "/qrcode.png",
+    RIDER_BUTTON: "/riderButton.png",
+    SCROLL: "/scroll.png",
+  },
+};
+
+export default ICONS;
